ea9ebba0d094c9223989650edd88eff4b9dec64d
Henry <hhburnsboy@gmail.com>
2018-02-21 13:53:33 -0500

    Updated Visualizer test cases

--- Assignment04/Visualizer.java
+++ Assignment04/Visualizer.java
@@ -5,18 +5,22 @@ public class Visualizer{
 		Elevator elev = new Elevator(15,5,10);
 		Line line = new Line("G", elev);
 		List<Passenger> pass = line.getLine();
+		Line line1 = new Line("6", elev);
+		List<Passenger> pass1 = line1.getLine();
+		Line line2 = new Line("B4", elev);
+		List<Passenger> pass2 = line2.getLine();
 		for(int i = 0; i < 6; i++)
-			pass.add(new Passenger("9", elev));
+			pass2.add(new Passenger("9", elev));
 		for(int i = 0; i <3 ; i++)
 			pass.add(new Passenger("3",elev));
 		for(int i = 0 ;i <3 ; i++)
-			pass.add(new Passenger("B5", elev));
-//		for(int i = 0; i <02; i++)
-//			pass.add(new Passenger("G", elev));
-//		for(int i = 0; i < 3; i++)
-//			pass.add(new Passenger("5", elev));
-//		for(int i = 0; i < 1; i++)
-//			pass.add(new Passenger("B2", elev));
+			pass1.add(new Passenger("B5", elev));
+		for(int i = 0; i < 2; i++)
+			pass2.add(new Passenger("G", elev));
+		for(int i = 0; i < 2; i++)
+			pass1.add(new Passenger("5", elev));
+		for(int i = 0; i < 1; i++)
+			pass.add(new Passenger("B2", elev));
 		showElev(elev); 
 		elev.getLines().get(elev.getCurrentFloor()).loadElevator(); //NEW
 		for(int i = 0; i < 35; i++) {
